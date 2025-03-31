e87debfd38e65afe8bbe3de945ed337954d2e573
jjxmonster <kuba.tarabasz@wp.pl>
2025-03-31 23:20:16 +0200

    feat: implemented base migrations

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,17 +1,41 @@
 import { ConfigService, ConfigModule } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+const env = process.env.NODE_ENV || 'development';
+const dotenv_path = path.resolve(process.cwd(), `.env.${env}`);
+const result = dotenv.config({ path: dotenv_path });
+
+if (result.error) {
+  console.error(result.error);
+}
+
+export class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): DataSourceOptions {
+    return {
+      type: 'postgres',
+      host: configService.get('DATABASE_HOST'),
+      port: parseInt(configService.get('DATABASE_PORT')),
+      username: configService.get('DATABASE_USERNAME'),
+      password: configService.get('DATABASE_PASSWORD'),
+      database: configService.get('DATABASE_NAME'),
+      synchronize: true,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      migrations: ['dist/database/migrations/*.{ts,js}'],
+    };
+  }
+}
 
 export const databaseConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
-  useFactory: (configService: ConfigService) => ({
-    type: 'postgres',
-    host: configService.get('DATABASE_HOST'),
-    port: parseInt(configService.get('DATABASE_PORT')),
-    username: configService.get('DATABASE_USERNAME'),
-    password: configService.get('DATABASE_PASSWORD'),
-    database: configService.get('DATABASE_NAME'),
-    autoLoadEntities: true,
-    synchronize: true,
-  }),
+  useFactory: (configService: ConfigService) =>
+    TypeOrmConfig.getOrmConfig(configService),
   inject: [ConfigService],
 };
+
+export default new DataSource({
+  ...TypeOrmConfig.getOrmConfig(new ConfigService()),
+  synchronize: false,
+});
---
+++ src/database/migrations/1743455715954-CreateIngredientTable.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateIngredientTable1743455715954 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'ingredient',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'amount',
+            type: 'decimal',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('ingredient');
+  }
+}
